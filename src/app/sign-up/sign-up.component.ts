f63177a495afb461097abcac6bd41ac26bd1443e
suji <suji@thebridgesoft.kr>
2017-09-29 11:07:39 +0900

    signup form buiding

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { User } from './../service/user';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent {
+  username: string;
+  emaul: string;
+  password: string;
 
   constructor() { }
 
-  ngOnInit() {
+  onSubmit() {
   }
-
 }
