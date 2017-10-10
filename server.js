7cbb1b08c282305ae8d83f196316ddbb7b4c2267
suji <suji@thebridgesoft.kr>
2017-10-10 16:59:12 +0900

    mongoDB 연동

--- server.js
+++ server.js
@@ -4,14 +4,25 @@
  * 
  */
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
 const app = express();
 
+//MongoDB와 상호 작용하기위한 API 파일
+const db_api = require('./server/routes/mongodb');
+
+// Parsers 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //절대 경로를 사용한 정적 파일 제공; angular output folder 등록
 app.use(express.static(path.join(__dirname, 'dist')));
 
-let port = 3000;
+// DB API location
+app.use('/api', db_api);
+
+let port = 4200;
 app.set('port', port);
 
 app.get('*', (req, res) => {
--- server/routes/mongodb.js
+++ server/routes/mongodb.js
@@ -2,4 +2,52 @@
  * Cinema\server\routes\mongodb.js
  * mongoDB connect 작업만 하고 그외 작업은 이 파일은 모듈로 받아서 작업해보쟈!
  */
+const express = require('express');
+const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 
+//connect
+const connection = (closure) => {
+    return MongoClient.connect('mongodb://localhost:27017/cinema', (err, db) => {
+        if(err) return console.log(`mongodb connect err [${err}]`);
+        closure(db);
+        console.log(`mongodb connect success!`);
+    });
+};
+
+//Error handling 오류처리
+const sendError = (err, res) => {
+    response.status = 501;
+    response.message = typeof err == 'object'? err.message : err;
+    res.status(501).json(response);
+};
+
+// Response handling 응답처리
+let response = {
+    status: 200,
+    data: [],
+    message: null
+};
+
+// Get users
+// url : http://localhost:4200/api/getusers 
+router.get('/getusers', (req, res) => {
+    connection((db) => {
+        db.collection('users')
+            .find()
+            .toArray()
+            .then((users) => {
+                response.data = users;
+                res.json(response);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
+});
+
+//insert users
+
+
+module.exports = router;
